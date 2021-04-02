36cd2728b1ef566b7f115aa69f3879c568769239
epho21 <39255242+rivaldoismir@users.noreply.github.com>
2021-04-02 17:04:01 +0700

    latihan

---
+++ Pratikum-5/src/latihan/Main.java
@@ -0,0 +1,9 @@
+package latihan;
+import java.util.Vector;
+
+class Somevector{
+    public static void main(String[] arg){
+        Vector<Integer> i = new Vector<>();
+        Vector<String> unggas
+    }
+}
